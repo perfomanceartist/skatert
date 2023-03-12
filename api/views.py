4882869fa1b089ec6b516b668e69f2bcf532445b
perfomanceartist <s.sorockoletov@ya.ru>
2023-03-12 19:13:26 +0300

    Отправка e-mail

--- api/views.py
+++ api/views.py
@@ -1,5 +1,24 @@
 from django.http import HttpResponse
-
+from django.http import HttpResponseBadRequest
+from django.conf import settings
+from django.core.mail import send_mail
 
 def index(request):
     return HttpResponse("API")
+
+
+
+def email_auth(request):
+    if request.method == "POST":
+        user = request.POST.get("email", "s.sorockoletov@ya.ru")    
+        subject = 'Skatert. Код Подтверждения входа'
+        ###
+        code = '123456'
+        ###
+        message = f'Код подтверждения для входа в Skatert: {code}'
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = [user, ]
+        send_mail( subject, message, email_from, recipient_list, fail_silently=False)
+        return HttpResponse("Success")
+    else:
+        return HttpResponseBadRequest("Некорректные данные")
--- skatert/settings.py
+++ skatert/settings.py
@@ -121,3 +121,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+#EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'skatertTeam@yandex.ru'
+EMAIL_HOST_PASSWORD = 'hcxfztfwgjrozwwy'  #password associated with above email-id
