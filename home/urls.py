1ae75cc25c4f09857b6dbea13dc90e1480a8b454
Ulkin Aleksey <ulkin.a@raidix.com>
2023-04-01 01:36:14 +0300

    SKATERT-5 Создать базовые страницы

--- home/urls.py
+++ home/urls.py
@@ -2,10 +2,10 @@ from django.urls import path, re_path
 
 from . import views
 
-urlpatterns = [    
+urlpatterns = [
     path('user/<slug:nickname>', views.user, name='user'),
     path('mypage', views.mypage, name='mypage'),
     path('login', views.login, name='login'),
     path('register', views.register, name='register'),
-    path('', views.index, name='index'),
+    path('', views.skatert, name='skatert'),
 ]
