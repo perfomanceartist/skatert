add28d54abb0dade50348f3512db03115dc6b778
Alexey Ulkin <aa-ulkin.alex@yandex.ru>
2023-04-03 06:04:32 +0300

    SKATERT-14 Составить js запросы к базовым страницам

--- api/views.py
+++ api/views.py
@@ -1,7 +1,6 @@
 import datetime
 import json
 from random import randint
-
 from django.conf import settings
 from django.core.mail import send_mail
 from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
