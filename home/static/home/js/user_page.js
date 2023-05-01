d70f9893f2483d0d9130cd3fddcecef1bd3b2809
yoursenpaiiiiii <92677584+yoursenpaiiiiii@users.noreply.github.com>
2023-05-01 18:53:03 +0300

    добавил дизайна на то что было в мейне
    здесь только функционал по регистрации/входу и пустая мейн страница
    
    добавил static папки для приложений, вынес в них css и js
    
    дизайн говна

---
+++ home/static/home/js/user_page.js
@@ -0,0 +1,10 @@
+async function exit_account() {
+    const response = await fetch("/api/logout", {
+    method: "POST",
+    headers: { "Accept": "application/json" }
+  });
+  if (response.ok === true) {
+    window.location.replace('/login');
+  }
+  else alert("Error!")
+}
---
+++ home/static/login/js/register_page.js
@@ -0,0 +1,46 @@
+async function register() {
+    var nickname = document.getElementById('loginControlInputNick').value;
+    console.log(nickname);
+    var email = document.getElementById('loginControlInputEmail').value;
+    var password = document.getElementById('loginControlInputPassword').value;
+    var passwordConfirm = document.getElementById('loginControlInputPasswordConfirm').value
+    document.getElementById('nickname_duplicate').classList.add('d-none');
+    document.getElementById('pass_mismatch').classList.add('d-none');
+    document.getElementById('empty_field').classList.add('d-none');
+    document.getElementById('incorrect_email').classList.add('d-none');
+
+    if (passwordConfirm !== password) {
+      document.getElementById('pass_mismatch').classList.remove('d-none');
+      document.getElementById('password').reset();
+      return;
+    }
+    // отправляет запрос и получаем ответ
+    const response = await fetch("/api/register", {
+        method: "POST",
+        headers: { "Accept": "application/json" },
+        body: JSON.stringify({
+            nickname: nickname,
+            email: email,
+            hash: password
+        })
+    });
+    if (response.status == 222) {  // nickname is not unique
+        document.getElementById('nickname_duplicate').classList.remove('d-none');
+        document.getElementById('nickname').reset();
+        return;
+    }
+    if (response.status == 223) {  // empty field(s)
+        document.getElementById('empty_field').classList.remove('d-none');
+        return;
+    }
+    if (response.status == 225) {  // empty field(s)
+        document.getElementById('incorrect_email').classList.remove('d-none');
+        document.getElementById('email').reset();
+        return;
+    }
+
+    if (response.ok === true) {
+        window.location.replace('/login');
+    }
+    else alert('Registration error!');
+}
