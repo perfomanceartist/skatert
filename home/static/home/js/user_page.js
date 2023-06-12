18cc4d8808679fbec76882714c7e04fae8fa56df
perfomanceartist <s.sorockoletov@ya.ru>
2023-06-12 23:41:42 +0300

    Перенос кнопки Выйти

--- home/static/home/js/user_page.js
+++ home/static/home/js/user_page.js
@@ -93,16 +93,7 @@ async function show_music(tracks, nickname, reaction = false) {
 }
 
 
-async function exit_account() {
-  const response = await fetch("/api/logout", {
-  method: "POST",
-  headers: { "Accept": "application/json" }
-});
-if (response.ok === true) {
-  window.location.replace('/login');
-}
-else alert("Error!")
-}
+
 
 
 async function import_tracks(nickname) {
