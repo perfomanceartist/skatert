5e59a4857f624d3c3406b21f6da7c69f2babc1ba
perfomanceartist <s.sorockoletov@ya.ru>
2023-06-25 09:00:25 +0300

    design fixes

--- home/static/home/js/user_page.js
+++ home/static/home/js/user_page.js
@@ -1,5 +1,5 @@
-//Если reaction == true, вывести треки с кнопками реакции (сердечком).
-async function show_music(tracks, nickname, reaction = false) {
+//Если dislike == true, вывести треки с кнопкой дислайка (крестиком).
+async function show_music(tracks, nickname, dislike = false) {
   for (var obj in tracks) {
     var track = tracks[obj];
     console.log(track);
@@ -42,53 +42,66 @@ async function show_music(tracks, nickname, reaction = false) {
     else{
       album_box.style.backgroundImage = "url(/static/home/img/no-cover.jpg)"; 
     }
-  
-  
-    if (reaction == true) {
-        /* КНОПКИ РЕАКЦИИ ПОЛЬЗОВАТЕЛЯ */
-        /* КНОПКА ЛАЙКА */
-      var heartCircle = document.createElement('div');
-      heartCircle.classList.add('heart-Circle');
-      heartCircle.setAttribute('role', 'checkbox');
-      heartCircle.setAttribute('aria-checked', 'false');
-      heartCircle.setAttribute('title', 'Нравится')
-      var leftSide = document.createElement('div');
-      leftSide.classList.add('left-Side', 'sides');
-      var rightSide = document.createElement('div');
-      rightSide.classList.add('right-Side', 'sides');
-      var heart1 = document.createElement('div');
-      heart1.classList.add('heart');
-      var half1 = document.createElement('div');
-      half1.classList.add('half');
-      half1.appendChild(heart1);
-      var heart2 = document.createElement('div');
-      heart2.classList.add('heart');
-      var half2 = document.createElement('div');
-      half2.classList.add('half');
-      half2.appendChild(heart2);
-      var heartContainer = document.createElement('div');
-      heartContainer.classList.add('heart-Container');
 
-      heartContainer.appendChild(leftSide);
-      heartContainer.appendChild(rightSide);
-      leftSide.appendChild(half1);
-      rightSide.appendChild(half2);
-      heartCircle.appendChild(heartContainer);
+    /* КНОПКИ РЕАКЦИИ ПОЛЬЗОВАТЕЛЯ */
+      /* КНОПКА ЛАЙКА */
+
+    var heartCircle = document.createElement('div');
+    heartCircle.classList.add('heart-Circle');
+    heartCircle.setAttribute('role', 'checkbox');
+    heartCircle.setAttribute('aria-checked', 'false');
+    heartCircle.setAttribute('title', 'Нравится')
+    var leftSide = document.createElement('div');
+    leftSide.classList.add('left-Side', 'sides');
+    var rightSide = document.createElement('div');
+    rightSide.classList.add('right-Side', 'sides');
+    var heart1 = document.createElement('div');
+    heart1.classList.add('heart');
+    var half1 = document.createElement('div');
+    half1.classList.add('half');
+    half1.appendChild(heart1);
+    var heart2 = document.createElement('div');
+    heart2.classList.add('heart');
+    var half2 = document.createElement('div');
+    half2.classList.add('half');
+    half2.appendChild(heart2);
+    var heartContainer = document.createElement('div');
+    heartContainer.classList.add('heart-Container');
+
+    heartContainer.appendChild(leftSide);
+    heartContainer.appendChild(rightSide);
+    leftSide.appendChild(half1);
+    rightSide.appendChild(half2);
+    heartCircle.appendChild(heartContainer);
+  
+  
+    if (dislike == true) {
+        /* КНОПКА ДИЗЛАЙКА */
+      var crossCircle = document.createElement('div');
+      crossCircle.classList.add('cross-Circle');
+      crossCircle.setAttribute('role', 'checkbox');
+      crossCircle.setAttribute('aria-checked', 'false');
+      crossCircle.setAttribute('title', 'Больше не рекомендовать')
+      var crossContainer = document.createElement('div');
+      crossContainer.classList.add('cross');
+
+      
+      crossCircle.appendChild(crossContainer);
     }
-  
-  
   
         /* СБОР ТРЕКА */
     element.appendChild(song_box);
-    if (reaction == true) {
-      element.appendChild(heartCircle);
+    element.appendChild(heartCircle);
+    if (dislike == true) {
+      element.appendChild(crossCircle);
     }
     element.appendChild(album_box);
     document.getElementById("user_tracks").appendChild(element);
   
   }
-  if (reaction == true) {
-    set_like_listeners(nickname);
+  set_like_listeners(nickname);
+  if (dislike == true) {
+    set_dislike_listeners(nickname);
   }
 }
 
@@ -143,44 +156,102 @@ show_music(tracks = tracks, nickname = nickname);
 
 async function like_handler(button, nickname){
 
-console.log(nickname);
-// console.log(button.value);
+  console.log(nickname);
+  // console.log(button.value);
 
-if( button.getAttribute('aria-checked') === "false"){
-  button.setAttribute('aria-checked', 'true');
-  button.setAttribute('title', 'Не нравится')
-}
-else{
-  button.setAttribute('aria-checked', 'false');
-  button.setAttribute('title', 'Нравится')
-}
-
-var track_box = button.closest(".list_element");
-var song_info = track_box.querySelector(".song_box");
-var song_name = song_info.querySelector(".song_name");
-var song_artist = song_info.querySelector(".song_artist");
-
-console.log(song_name.innerText)
-
-const response_like_handler = await fetch("/music/clickLike/", {
-  method: "POST",
-  headers: { "Accept": "application/json" },
-  body: JSON.stringify({
-    nickname : nickname,
-    song_name : song_name.innerText,
-    song_artist : song_artist.innerText
-    })
-  });
-
-  console.log(response_like_handler);
-  if (response_like_handler.ok === true) {
-    return true;
+  if( button.getAttribute('aria-checked') === "false"){
+    button.setAttribute('aria-checked', 'true');
+    button.setAttribute('title', 'Не нравится')
   }
-  else {
-    const text = await response_like_handler.text();
-    return false;
+  else{
+    button.setAttribute('aria-checked', 'false');
+    button.setAttribute('title', 'Нравится')
   }
+
+  var track_box = button.closest(".list_element");
+  var song_info = track_box.querySelector(".song_box");
+  var song_name = song_info.querySelector(".song_name");
+  var song_artist = song_info.querySelector(".song_artist");
+
+  //УБЕДИТЬСЯ ЧТО ОН УДАЛЯЕТСЯ ИЗ дизлайкнутых
+  var cross = track_box.querySelector(".cross-Circle")
+  if (cross != null){
+    cross.setAttribute('aria-checked', 'false');
+    cross.setAttribute('title', 'Нравится')
+  }
+
+
+  console.log(song_name.innerText)
+
+  const response_like_handler = await fetch("/music/clickLike/", {
+    method: "POST",
+    headers: { "Accept": "application/json" },
+    body: JSON.stringify({
+      nickname : nickname,
+      song_name : song_name.innerText,
+      song_artist : song_artist.innerText
+      })
+    });
+
+    console.log(response_like_handler);
+    if (response_like_handler.ok === true) {
+      return true;
+    }
+    else {
+      const text = await response_like_handler.text();
+      return false;
+    }
 }
+
+
+async function dislike_handler(button, nickname){
+
+  console.log(nickname);
+
+  if( button.getAttribute('aria-checked') === "false"){
+    button.setAttribute('aria-checked', 'true');
+    button.setAttribute('title', 'Рекомендовать')
+  }
+  else{
+    button.setAttribute('aria-checked', 'false');
+    button.setAttribute('title', 'Больше не рекомендовать')
+  }
+
+  var track_box = button.closest(".list_element");
+  var song_info = track_box.querySelector(".song_box");
+  var song_name = song_info.querySelector(".song_name");
+  var song_artist = song_info.querySelector(".song_artist");
+
+
+  //УБЕДИТЬСЯ ЧТО ОН УДАЛЯЕТСЯ ИЗ БИБЛИОТЕКИ
+  var heart = track_box.querySelector(".heart-Circle")
+  heart.setAttribute('aria-checked', 'false');
+  heart.setAttribute('title', 'Нравится')
+
+
+  console.log(song_name.innerText)
+
+  const response_dislike_handler = await fetch("/music/clickDislike/", {
+    method: "POST",
+    headers: { "Accept": "application/json" },
+    body: JSON.stringify({
+      nickname : nickname,
+      song_name : song_name.innerText,
+      song_artist : song_artist.innerText
+      })
+    });
+
+    console.log(response_dislike_handler);
+    if (response_dislike_handler.ok === true) {
+      return true;
+    }
+    else {
+      const text = await response_dislike_handler.text();
+      return false;
+    }
+}
+
+
 
 async function get_recomendations(nickname) {
 
@@ -204,7 +275,7 @@ async function get_recomendations(nickname) {
   document.getElementById("user_recomendations").innerHTML = "";
 
 
-  show_music(tracks = tracks, nickname = nickname, reaction = true);
+  show_music(tracks = tracks, nickname = nickname, dislike = true);
 }
 
 async function set_like_listeners(nickname){
@@ -217,6 +288,20 @@ async function set_like_listeners(nickname){
         like_handler(button, nickname);
       }
     })(hrts[i]));
+  }
+
+}
+
+async function set_dislike_listeners(nickname){
+
+  var crsss = document.querySelectorAll('.cross-Circle');
+
+  for (var i = 0; i < crsss.length; i++) {
+    crsss[i].addEventListener("click", (function(button) {
+      return function() {
+        dislike_handler(button, nickname);
+      }
+    })(crsss[i]));
   }
 
 }
@@ -239,6 +324,12 @@ async function setup_user_page(nickname){
   document.getElementById("user_recomendations").innerHTML = "";
 
   show_music(tracks = tracks, nickname = nickname);
+
+  var hrts = document.querySelectorAll('.heart-Circle');
+  for (var i = 0; i < hrts.length; i++) {
+    hrts[i].setAttribute('aria-checked', 'true');
+    hrts[i].setAttribute('title', 'Не нравится')
+  }
 
 }
 
@@ -266,7 +357,7 @@ async function get_search_results(nickname) {
   var tracks = await response_recomendation.json();
 
   // console.log(tracks)
-  show_music(tracks = tracks, nickname = nickname, reaction = true);
+  show_music(tracks = tracks, nickname = nickname);
 
   var hrts = document.querySelectorAll('.heart-Circle');
   for (var i = 0; i < hrts.length; i++) {
